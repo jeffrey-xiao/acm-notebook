014ee340f5ce8d4209db94a4c580bfe8bb1158bd
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-10-13 11:23:15 -0400

    Added KMP, Manachers, Suffix_Array, and Z_Algorithm

---
+++ code/KMP.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct KMP {
+    string pattern;
+    vector<int> lcp;
+    KMP (string pattern): pattern(pattern), lcp(pattern.size()) {
+        buildLcp();
+    }
+
+    void buildLcp () {
+        for (int i = 1; i < (int)pattern.size(); i++) {
+            int j = lcp[i - 1];
+            while (j > 0 && pattern[j] != pattern[i])
+                j = lcp[j - 1];
+            if (pattern[j] == pattern[i])
+                j++;
+            lcp[i] = j;
+        }
+        for (int i = 0; i < pattern.size(); i++)
+            printf("%d\n", lcp[i]);
+    }
+
+    int search (string text) {
+        int j = 0;
+        for (int i = 0; i < (int)text.size(); i++) {
+            while (j > 0 && text[i] != pattern[j])
+                j = lcp[j - 1];
+            if (text[i] == pattern[j])
+                j++;
+            if (j == (int)pattern.size())
+                return i - j + 1;
+        }
+        return -1;
+    }
+};

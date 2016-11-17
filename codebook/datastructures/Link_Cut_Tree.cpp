f862d997a2c6be729b2f4d33399b98f2f6cade17
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-11-17 00:45:49 -0500

    Updated

--- codebook/datastructures/Link_Cut_Tree.cpp
+++ codebook/datastructures/Link_Cut_Tree.cpp
@@ -4,24 +4,156 @@ using namespace std;
 
 struct LinkCut {
     struct Node {
-        int val;
-        Node *child[2], *par;
-        Node (int val): val(val) {
+        int sz;
+        Node *child[2], *par, *pathPar;
+        Node (int sz): sz(sz) {
             child[0] = this;
             child[1] = this;
             par = this;
+            pathPar = this;
         }
     };
 
     static Node *null;
-    Node *root;
     vector<Node*> nodes;
 
-    LinkCut (int N): nodes(N) {}
+    LinkCut (int N): nodes(N) {
+        for (int i = 0; i < N; i++) {
+            nodes[i] = new Node(1);
+            nodes[i]->child[0] = nodes[i]->child[1] = nodes[i]->par = nodes[i]->pathPar = null;
+        }
+    }
 
-    void access (Node* u) {
+    static void update (Node *u) {
+        if (u == null)
+            return;
+        u->sz = 1 + u->child[0]->sz + u->child[1]->sz;
+    }
 
+    static int getDir (Node *u, Node *p) {
+        return p->child[0] == u ? 0 : 1;
+    }
+
+    static Node* rotate (Node* u, int dir) {
+        Node *c = u->child[dir ^ 1], *p = u->par, *pp = p->par;
+        p->child[dir] = c;
+        c->par = p;
+        u->child[dir ^ 1] = p;
+        p->par = u;
+        u->par = pp;
+        pp->child[getDir(p, pp)] = u;
+
+        u->pathPar = p->pathPar;
+        p->pathPar = null;
+        update(p);
+        update(u);
+        update(pp);
+        return u;
+    }
+
+    void splay (Node *u) {
+        while (u->par != null) {
+            Node *p = u->par, *pp = p->par;
+            int dp = getDir(u, p), dpp = getDir(p, pp);
+            if (pp == null) rotate(u, dp);
+            else if (dp == dpp) rotate(p, dpp), rotate(u, dp);
+            else rotate(u, dp), rotate(u, dpp);
+        }
+    }
+
+    Node* access (Node* u) {
+        splay(u);
+        if (u->child[1] != null) {
+            u->child[1]->pathPar = u;
+            u->child[1]->par = null;
+            u->child[1] = null;
+            update(u);
+        }
+        Node* w = u;
+        while (u->pathPar != null) {
+            Node *v = u->pathPar;
+            splay(v);
+            if (v->child[1] != null) {
+                v->child[1]->pathPar = v;
+                v->child[1]->par = null;
+            }
+            v->child[1] = u;
+            update(v);
+            u->par = v;
+            u->pathPar = null;
+            u = v;
+        }
+        splay(w);
+        return w;
+    }
+
+    // precondition: n must be a root node, and n and m must be in different trees
+    void link (Node *n, Node *m) {
+        n = access(n);
+        m = access(m);
+        n->child[0] = m;
+        m->par = n;
+        update(n);
+    }
+
+    // precondition: n must not be a root node
+    void cut (Node *n) {
+        n = access(n);
+        if (n->child[0] != null) {
+            n->child[0]->par = null;
+            n->child[0] = null;
+        }
+        update(n);
+    }
+
+    Node* getRoot (Node *n) {
+        n = access(n);
+        while (n->child[0] != null)
+            n = n->child[0];
+        n = access(n);
+        return n;
+    }
+
+    int getHeight (Node *n) {
+        access(n);
+        return n->child[0]->sz + 1;
     }
 };
 
 LinkCut::Node* LinkCut::null = new Node(0);
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+int main () {
+    int N, M;
+    scan(N);
+    scan(M);
+
+    LinkCut t(N);
+
+    for (int i = 0; i < M; i++) {
+        char in[5] ;
+        int j = 0, ch;
+        while( ( ( ch = getchar() ) != '\n' ) && ( ch != ' ' ) && ( j < ( 5 - 1 ) ) )
+           in[j++]=ch;
+        in[j]='\0';
+        int u, v;
+        scan(u);
+        scan(v);
+        u--, v--;
+        if (in[0] == 'a') {
+            t.link(t.getRoot(t.nodes[u]), t.nodes[v]);
+        }
+        else if (in[0] == 'r') {
+            if (t.nodes[v]->par == t.nodes[u] || t.nodes[v]->pathPar == t.nodes[u])
+                t.cut(t.nodes[v]);
+            else
+                t.cut(t.nodes[u]);
+        } else {
+            if (t.getRoot(t.nodes[u]) == t.getRoot(t.nodes[v]))
+                puts("YES");
+            else
+                puts("NO");
+        }
+    }
+    return 0;
+}
